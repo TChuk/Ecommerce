76c966f3f4e2efa64a633932a7debe8242b5e1af
Ryan Tabachuk <neji@cyrena.ca>
2017-03-20 21:10:45 +0000

    Added some styles, added model validations for the products model, fixed the routes, and updated the products views.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :system
+
+  validates :name, :price, :stock_quantity, :system_id, :category_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'product/index'
+  get '/', to: 'products#index', as: 'index'
 
-  get 'product/show'
+  get 'products/:id' => 'products#show', as: 'product', id: /\d+/
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
